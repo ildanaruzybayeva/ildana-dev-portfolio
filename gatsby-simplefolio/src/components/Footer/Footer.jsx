2755642a98516d917c741c31ac9fc7507fc1e3d2
Ildana <ildana.ruzybayeva@gmail.com>
2020-06-28 13:42:01 +0000

    Added projects and descriptions

--- gatsby-simplefolio/src/components/Footer/Footer.jsx
+++ gatsby-simplefolio/src/components/Footer/Footer.jsx
@@ -38,9 +38,9 @@ const Footer = () => {
         </div>
         <hr />
         <p className="footer__text">
-          © {new Date().getFullYear()} - Template developed by{' '}
-          <a href="https://github.com/cobidev" target="_blank" rel="noopener noreferrer">
-            Jacobo Martínez
+          © {new Date().getFullYear()} - Portfolio by{' '}
+          <a href="https://github.com/ildanaruzybayeva" target="_blank" rel="noopener noreferrer">
+            Ildana Ruzybayeva
           </a>
         </p>
 
--- gatsby-simplefolio/src/mock/data.js
+++ gatsby-simplefolio/src/mock/data.js
@@ -17,7 +17,7 @@ export const heroData = {
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpg',
+  img: 'profile.PNG',
   paragraphOne: '',
   paragraphTwo: '',
   paragraphThree: '',
@@ -28,30 +28,33 @@ export const aboutData = {
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'tretton37.PNG',
+    title: '_tretton37-ninjas',
+    info:
+      'React hooks project based on public API from tretton37. Displays all employees with names, office and social media links. Main features: filter by office, pagination, name sort. Fully responsive and written without any UI frameworks. Deployed on Netlify',
+    info2: '#javascript #reacthooks #css #gitpod',
+    url: 'https://tretton37-ninjas.netlify.app/',
+    repo: 'https://github.com/ildanaruzybayeva/tretton37-ninjas', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'messageup.PNG',
+    title: 'Message-up',
+    info:
+      'Real-time chat application based on socket.io. Features: create a room, join/leave a room, real time messaging. Client is written in React while backend with Node/Express Responsive and built wihtout any UI frameworks. Deployed on Heroku and Netlify',
+    info2: '#reacthooks #nodejs #express #socket.io #css',
+    url: 'https://message-up.netlify.app/',
+    repo: 'https://github.com/ildanaruzybayeva/messenger-react-nodejs', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'ltmfs.PNG',
+    title: 'Letter to future self',
+    info:
+      'The app allows to schedule and send a Letter to your future self. Client built with react and server with nodejs. The app is not deployed due to personal email being used as the main sender. Feel free to browse the code nevertheless',
+    info2: '#reacthooks #nodejs #express #nodemailer #google-OAuth2',
+    url: 'https://github.com/ildanaruzybayeva/letter-future-self-node-react',
+    repo: 'https://github.com/ildanaruzybayeva/letter-future-self-node-react', // if no repo, the button will not show up
   },
 ];
 
@@ -59,7 +62,7 @@ export const projectsData = [
 export const contactData = {
   cta: '',
   btn: '',
-  email: '',
+  email: 'ildana.ruzybayeva@gmail.com',
 };
 
 // FOOTER DATA
@@ -68,27 +71,22 @@ export const footerData = {
     {
       id: nanoid(),
       name: 'twitter',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'codepen',
-      url: '',
+      url: 'https://twitter.com/JamsJavascript',
     },
     {
       id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/ruzybayeva/',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/ildanaruzybayeva',
     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
